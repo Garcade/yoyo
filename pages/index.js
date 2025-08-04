77042c90d08904c08c413998c3990677db5f37ff
Aditya Mathur <aditya94mathur@gmail.com>
2025-08-04 23:55:45 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Welcome to GArcade!</h1>
+      <p>Your Next.js site is working.</p>
+    </div>
+  );
+}
